e02336e8dbf61537a5cc577baa86006f499075d3
azu <azuciao@gmail.com>
2021-04-25 12:20:13 +0900

    fix(deps): update to kuromojin@3

--- src/no-doubled-conjunction.js
+++ src/no-doubled-conjunction.js
@@ -1,9 +1,9 @@
 // LICENSE : MIT
 "use strict";
-import {RuleHelper} from "textlint-rule-helper";
-import {getTokenizer} from "kuromojin";
-import {split as splitSentences, Syntax as SentenceSyntax} from "sentence-splitter";
-import StringSource from "textlint-util-to-string";
+import { RuleHelper } from "textlint-rule-helper";
+import { getTokenizer } from "kuromojin";
+import { split as splitSentences, Syntax as SentenceSyntax } from "sentence-splitter";
+import { StringSource } from "textlint-util-to-string";
 
 /*
     1. Paragraph Node -> text
@@ -15,9 +15,9 @@ import StringSource from "textlint-util-to-string";
  */
 export default function (context, options = {}) {
     const helper = new RuleHelper(context);
-    const {Syntax, report, getSource, RuleError} = context;
+    const { Syntax, report, getSource, RuleError } = context;
     return {
-        [Syntax.Paragraph](node){
+        [Syntax.Paragraph](node) {
             if (helper.isChildNode(node, [Syntax.Link, Syntax.Image, Syntax.BlockQuote, Syntax.Emphasis])) {
                 return;
             }
@@ -29,39 +29,39 @@ export default function (context, options = {}) {
             }).filter(isSentenceNode);
             // if not have a sentence, early return
             // It is for avoiding error of emptyArray.reduce().
-            if(sentences.length === 0) {
+            if (sentences.length === 0) {
                 return;
             }
             return getTokenizer().then(tokenizer => {
-              const selectConjenction = (sentence) => {
-                let tokens = tokenizer.tokenizeForSentence(sentence.raw);
-                let conjunctionTokens = tokens.filter((token) => token.pos === "接続詞");
-                return [sentence, conjunctionTokens];
-              };
-              let prev_token = null;
-              sentences.map(selectConjenction).reduce((prev, current) => {
-                let token = prev_token;
-                let [sentence, current_tokens] = current;
-                let [prev_sentence, prev_tokens] = prev;
-                if (prev_tokens && prev_tokens.length > 0) {
-                  token = prev_tokens[0];
-                }
-                if (current_tokens.length > 0) {
-                  if (token && current_tokens[0].surface_form === token.surface_form) {
-                    let originalIndex = source.originalIndexFromPosition({
-                      line: sentence.loc.start.line,
-                      column: sentence.loc.start.column + (current_tokens[0].word_position - 1)
-                    });
-                    // padding position
-                    var padding = {
-                        index: originalIndex
-                    };
-                    report(node, new RuleError(`同じ接続詞が連続して使われています。`, padding));
-                  }
-                }
-                prev_token = token;
-                return current;
-              });
+                const selectConjenction = (sentence) => {
+                    const tokens = tokenizer.tokenizeForSentence(sentence.raw);
+                    const conjunctionTokens = tokens.filter((token) => token.pos === "接続詞");
+                    return [sentence, conjunctionTokens];
+                };
+                let prev_token = null;
+                sentences.map(selectConjenction).reduce((prev, current) => {
+                    const [sentence, current_tokens] = current;
+                    const [prev_sentence, prev_tokens] = prev;
+                    let token = prev_token;
+                    if (prev_tokens && prev_tokens.length > 0) {
+                        token = prev_tokens[0];
+                    }
+                    if (current_tokens.length > 0) {
+                        if (token && current_tokens[0].surface_form === token.surface_form) {
+                            const originalIndex = source.originalIndexFromPosition({
+                                line: sentence.loc.start.line,
+                                column: sentence.loc.start.column + (current_tokens[0].word_position - 1)
+                            });
+                            // padding position
+                            const padding = {
+                                index: originalIndex
+                            };
+                            report(node, new RuleError(`同じ接続詞が連続して使われています。`, padding));
+                        }
+                    }
+                    prev_token = token;
+                    return current;
+                });
             });
         }
     }

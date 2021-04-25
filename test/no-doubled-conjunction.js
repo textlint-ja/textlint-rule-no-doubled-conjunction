5b8903e3e69e2b4c64c9c5747c512c8087035418
azu <azu@users.noreply.github.com>
2021-04-25 12:32:03 +0900

    fix(deps): update to kuromojin@3 and 全角ピリオド対応 (#8)
    ## Summary
    
    - Update to [kuromojin@3](https://github.com/azu/kuromojin/releases/tag/v3.0.0) and improve analysis.
    - 全角ピリオド（`．`）の対応 fix #6 
    - エラーメッセージの改善
    
    ## Features
    
    - 全角ピリオド（`．`）の対応 fix #6 
    - エラーメッセージの改善
    
    ##  Bug Fixes
    
    - Update to [kuromojin@3](https://github.com/azu/kuromojin/releases/tag/v3.0.0)
      - refs https://github.com/azu/kuromojin/issues/8
    - Update to [sentence-splitter@3](https://github.com/azu/sentence-splitter/releases/tag/3.0.0)
    
    ## CI
    
    - Move to GitHub Actions

--- test/no-doubled-conjunction.js
+++ test/no-doubled-conjunction.js
@@ -1,5 +1,6 @@
 import rule from "../src/no-doubled-conjunction";
 import TextLintTester from "textlint-tester";
+
 var tester = new TextLintTester();
 tester.run("no-doubled-conjunction", rule, {
     valid: [
@@ -9,39 +10,47 @@ tester.run("no-doubled-conjunction", rule, {
         "![](path/to/image.png)"// empty image label
     ],
     invalid: [
-      {
-          text: "朝起きた。そして昼は仕事をした。そして夜に寝た",
-          errors: [
-              {
-                  message: `同じ接続詞が連続して使われています。`,
-                  // last match
-                  line: 1,
-                  column: 17
-              }
-          ]
-      },
-      {
-          text: "そして朝起きた。昼は仕事をした。そして夜に寝た",
-          errors: [
-              {
-                  message: `同じ接続詞が連続して使われています。`,
-                  // last match
-                  line: 1,
-                  column: 17
-              }
-          ]
-      },
-      {
-          text: "かな漢字変換により漢字が多用される傾向がある。しかし漢字の多用が読みにくさをもたらす側面は否定できない。しかし、平仮名が多い文は間延びした印象を与える恐れもある。",
-          errors: [
-              {
-                  message: `同じ接続詞が連続して使われています。`,
-                  // last match
-                  line: 1,
-                  column: 53
-              }
-          ]
-      }
-
+        {
+            text: "朝起きた。そして昼は仕事をした。そして夜に寝た",
+            errors: [
+                {
+                    message: `同じ接続詞（そして）が連続して使われています。`,
+                    // last match
+                    line: 1,
+                    column: 17
+                }
+            ]
+        },
+        {
+            text: "そして朝起きた。昼は仕事をした。そして夜に寝た",
+            errors: [
+                {
+                    message: `同じ接続詞（そして）が連続して使われています。`,
+                    // last match
+                    line: 1,
+                    column: 17
+                }
+            ]
+        },
+        {
+            text: "かな漢字変換により漢字が多用される傾向がある。しかし漢字の多用が読みにくさをもたらす側面は否定できない。しかし、平仮名が多い文は間延びした印象を与える恐れもある。",
+            errors: [
+                {
+                    message: `同じ接続詞（しかし）が連続して使われています。`,
+                    // last match
+                    line: 1,
+                    column: 53
+                }
+            ]
+        },
+        {
+            text: "かな漢字変換により漢字が多用される傾向がある．しかし漢字の多用が読みにくさをもたらす側面は否定できない. しかし、平仮名が多い文は間延びした印象を与える恐れもある。",
+            errors: [
+                {
+                    message: `同じ接続詞（しかし）が連続して使われています。`,
+                    index: 53
+                }
+            ]
+        }
     ]
 });
